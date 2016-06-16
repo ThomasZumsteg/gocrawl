6e2cbd4d111c7832a42db27d80960c443245a90c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-06-16 16:12:57 -0700

    Can read from stdin
    Some file reorg to match

---
+++ gocrawl/gocrawl.go
@@ -0,0 +1,27 @@
+package gocrawl
+
+import (
+    "fmt"
+)
+
+
+// TestVersion is the version that the unit tests are run against
+const TestVersion = 1
+
+// Device is a network device that sends and receives commands
+type Device struct {
+	name string
+	ip   string
+}
+
+// NewDevice creates a new device
+func NewDevice(name, ip string) *Device {
+	return &Device{name: name, ip: ip}
+}
+
+func (dev *Device) GetUserAndPassword() {
+    fmt.Printf("Username: ")
+    var input string
+    fmt.Scanln(&input)
+    fmt.Println(input)
+}
