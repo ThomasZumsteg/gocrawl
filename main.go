9d85edfeffb59753d60fe37bd319f0b463f4153b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-06-18 23:19:21 -0700

    Added some logging

--- main.go
+++ main.go
@@ -4,25 +4,37 @@ import (
 	// "./gocrawl"
 	// "encoding/json"
 	"flag"
-	"fmt"
-	// "log"
+	// "fmt"
+    "io"
+	"log"
+    "os"
 )
+
 
 func main() {
 	var backgroundPtr = flag.Bool("background", false, "Runs the command in the background")
 	var databasePtr = flag.String("database", "./crawlnet", "Use this specified database")
 	var verbosePtr = flag.Bool("verbose", false, "Run in verbose mode")
 	var debugPtr = flag.Int("debug", 0, "Set the debug level (Default is 0)")
-
+    var logFile = flag.String("log", "", "Set the log file location")
 	flag.Parse()
 
-	fmt.Println("Background in: ", *backgroundPtr)
-	fmt.Println("Database is: ", *databasePtr)
-	fmt.Println("Verbose is: ", *verbosePtr)
-	fmt.Println("Debug level is: ", *debugPtr)
-	fmt.Println("Roots are:")
+    var logWriter io.Writer 
+    switch {
+    case *logFile == "":
+            logWriter = os.Stdout
+    }
+
+
+    logger := log.New(logWriter, "main: ", log.LstdFlags)
+
+	logger.Print("Background in: ", *backgroundPtr)
+	logger.Print("Database is: ", *databasePtr)
+	logger.Print("Verbose is: ", *verbosePtr)
+	logger.Print("Debug level is: ", *debugPtr)
+	logger.Print("Roots are:")
 	for _, root := range flag.Args() {
-		fmt.Printf("\t%s\n", root)
+		logger.Printf("\t%s", root)
 	}
 
 	// If Database isn't a file, confirm overwrite
